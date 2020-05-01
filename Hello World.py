af669b32c5aa74bf9fcfbc4e030a69b6175cc08d
tpn-git <64343268+tpn-git@users.noreply.github.com>
2020-04-30 23:04:28 -0400

    Update Hello World.py
    Edited Comments (Online & Local)

--- Hello World.py
+++ Hello World.py
@@ -2,10 +2,10 @@
 var = "Hello World"
 print(var)
 
-# Edited in GitHub Repo
+# Edited in GitHub Repo (Online)
 gh = "I'm edited in pycharm-github-repo"
 print(gh)
 
-# Edited in PyCharm IDE
+# Edited in PyCharm IDE (Local)
 pc = "I'm edited in PyCharm IDE locally"
 print(pc)
