7cc1563c337cb8af644a111909b68aef7dbac1ad
tpn-git <tpn.patel@gmail.com>
2020-05-01 01:33:14 -0400

    Edited Comments Section via PyCharm IDE

--- Hello World.py
+++ Hello World.py
@@ -9,3 +9,18 @@ print(gh)
 # Edited in PyCharm IDE (Local)
 pc = "I'm edited in PyCharm IDE locally"
 print(pc)
+
+
+# this is a single-line comment
+x = 3  # this is another single-line comment
+
+"""
+Triple-quoted strings that are not docstring (first thing in
+class/function/module) and are not assigned to variables are
+as comments ignored.
+"""
+
+'''
+Triple-quoted strings can use single or double quote charcaters
+without semantic difference
+'''
